9ab46e9487a85e6728b082dbf8b2e7bb124a7b43
Brenner Lopes <brenner@cursos7.com.br>
2017-05-31 14:15:58 -0300

    Colocando a função thumbnails e melhorando a estrutura do front-end no site

--- header.php
+++ header.php
@@ -1,7 +1,12 @@
 <!doctype html>
 <html>
 <head>
+    <?php $home = get_template_directory_uri(); ?>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="<?= 
+                    $home; ?>/reset.css"> 
+    <link rel="stylesheet" type="text/css" href="<?= 
+                    $home; ?>/style.css"> <!-- busca o diretório raiz do tema --> 
     <?php wp_head(); ?>
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php get_header(); ?>
-    <h1>Bem vindo</h1>
-<?php
-    if( have_posts() ){
-        while(have_posts() ){
+    
+
+
+<main class="home-main">
+    <div class="container">
+        <h1>Bem vindo</h1>
+        <ul class="imoveis-listagem">
+            <?php
+                if( have_posts() ){ /* Faz a busca dos posts que estão no banco */ 
+                while(have_posts() ){
             the_post();
 ?>
-  <h2><?php the_title(); ?></h2> <--- imprime o titulo do post -->
-</--->
- <div><?php the_content(); ?></div> <--- imprime a descrição do post /--->
-<?php
+            <!-- Faz a renderização de um item da lista de post ---> 
+            <li class="imoveis-listagem-item">
+                
+                 <?php the_post_thumbnail() ; ?> <!-- Busca a imagem salva no banco de dados --> 
+                <h2><?php the_title(); ?></h2> <!-- imprime o titulo do post -->
+                <p><?php the_content(); ?></p> <!-- imprime a descrição do post -->
+            </li>
             
-   }
-    }
+            <?php
+           }
+        }?>
+            
+        
+        </ul>
+        
+    </div>
+</main>
 
-?>
+
+
  <?php get_footer(); ?>
